e379f3d7fa36d31a78a1ec7c74df98b5c2cee62b
SeeLead <dendy.mailbox@gmail.com>
2026-09-05 10:01:39 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dd-grid-xau-v2";
+const CACHE = "dd-grid-xau-v5";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
